0d9de947ee9b5b81835e6ce696cd693b1f54fa84
suryaelidanto <suryaelidanto@gmail.com>
2024-10-18 11:00:31 +0700

    feat: count post blog time

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -40,10 +40,13 @@ function renderBlog() {
               >
             </h1>
             <div class="detail-blog-content">
-                ${blogs[index].createdAt} | Ichsan Emrald Alamsyah
+                ${getFullTime(blogs[index].createdAt)} | Ichsan Emrald Alamsyah
             </div>
             <p>
               ${blogs[index].content}
+            </p>
+            <p>
+                ${getDistanceTime(blogs[index].createdAt)}
             </p>
           </div>
         </div>`;
@@ -56,3 +59,61 @@ function deleteBlog(index) {
   blogs.splice(index, 1);
   renderBlog();
 }
+
+function getFullTime(date) {
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  const tanggal = date.getDate();
+  const bulan = months[date.getMonth()];
+  const tahun = date.getFullYear();
+
+  let jam = date.getHours();
+  let menit = date.getMinutes();
+
+  if (jam < 10) {
+    jam = "0" + jam;
+  }
+
+  if (menit < 10) {
+    menit = "0" + menit;
+  }
+
+  return `${tanggal} ${bulan} ${tahun} ${jam}:${menit} WIB`;
+}
+
+function getDistanceTime(timePost) {
+  const timeNow = new Date();
+  const distance = timeNow - timePost; // hasilnya miliseconds -> 1000ms = 1 detik
+
+  const seconds = Math.floor(distance / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const day = Math.floor(hours / 24);
+
+  if (seconds < 60) {
+    return `${seconds} seconds ago`;
+  } else if (minutes < 60) {
+    return `${minutes} minutes ago`;
+  } else if (hours < 60) {
+    return `${hours} hours ago`;
+  } else if (day < 24) {
+    return `${day} day ago`;
+  }
+}
+
+setInterval(() => {
+  renderBlog()
+}, 1000)
