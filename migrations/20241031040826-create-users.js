bb515edf2b92c867591f9585fcf6d841326a0d24
suryaelidanto <suryaelidanto@gmail.com>
2024-11-06 11:42:44 +0700

    chore: update migration createdAt updatedAt defaultValue

--- migrations/20241031040826-create-users.js
+++ migrations/20241031040826-create-users.js
@@ -21,12 +21,12 @@ module.exports = {
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.NOW,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
       },
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.NOW,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
       },
     });
   },
